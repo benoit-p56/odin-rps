e7091e49cc305acae0073d82a0cfe7412382452c
benoit-p56 <128241730+benoit-p56@users.noreply.github.com>
2024-05-25 19:37:17 -0400

    Wrote the getComputerChoice() and getHumanChoice() functions
    These are needed to get the computer's choice and the user's input

--- rps.js
+++ rps.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3);
+
+    if (x === 0) {
+        return `rock`;
+    }
+    else if (x === 1) {
+        return `paper`;
+    }
+    else {
+        return `scissors`;
+    }
+}
+
+
+function getHumanChoice() {
+    let choice = prompt("Enter a choice").toLowerCase();
+    
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        console.log(choice);
+        return choice;
+    }
+    else {
+        console.log("Please enter a valid choice");
+        getHumanChoice();
+    }
+}
+
+
